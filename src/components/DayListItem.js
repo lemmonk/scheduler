72d5a2af4a2930b1b27655ccc3c1358ccd280592
Kyle Lemmon <kylelemmon@Kyles-MacBook-Air.local>
2021-03-15 21:16:05 -0700

    add DayListItem styles with props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,24 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classnames from "classnames";
+
 
 export default function DayListItem(props) {
+
+  // const buttonClass = classnames("button", {
+  //   "button--confirm": props.confirm,
+  //   "button--danger": props.danger
+  // });
+
+  const dayClass = classnames("day-list__item",{
+    "day-list__item--selected": props.selected,
+    "day-list__item--full":props.full
+  });
+
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
